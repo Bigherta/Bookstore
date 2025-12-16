4c2f6b84df17e30a203f54e8269fd9b9209d9242
SEED <hhs_sjtu@sjtu.edu.cn>
2025-12-16 18:16:28 +0800

    update: find error in show module add breaking point more precisely

--- src/parser.cpp
+++ src/parser.cpp
@@ -303,9 +303,10 @@ void Parser::execute(const std::string &line_raw, UserManager &userManager, log 
         }
 
         case SHOW: {
-            assert(false);
+            
             if (tokens_.size() == 1)
             {
+                assert(false);
                 if (userManager.getCurrentUser().privilegeLevel < 1)
                 {
                     std::cout << "Invalid\n";
