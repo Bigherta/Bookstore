e518dd47d9b42513e913eca9ca97aa5be5bd7b95
SEED <hhs_sjtu@sjtu.edu.cn>
2025-12-17 22:02:30 +0800

    fixed : delete two file

--- src/tempCodeRunnerFile.cpp
+++
@@ -1,5 +0,0 @@
-return (code >= 0x4E00 && code <= 0x9FFF) || (code >= 0x3400 && code <= 0x4DBF) ||
-           (code >= 0x20000 && code <= 0x2A6DF) || (code >= 0x2A700 && code <= 0x2B73F) ||
-           (code >= 0x2B740 && code <= 0x2B81F) || (code >= 0x2B820 && code <= 0x2CEAF) ||
-           (code >= 0x2CEB0 && code <= 0x2EBEF) || (code >= 0x30000 && code <= 0x3134F) ||
-           (code >= 0x31350 && code <= 0x323AF);
